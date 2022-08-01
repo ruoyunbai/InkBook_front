b1362ea8093cef3522dd29a5a1f302b1c145ef7e
gong-wenxin <1153712410@qq.com>
2022-08-01 12:02:11 +0800

    auto push

--- src/components/Tourist/SimpleHeader/index.vue
+++ src/components/Tourist/SimpleHeader/index.vue
@@ -3,17 +3,15 @@
     <n-grid :cols="36">
       <n-gi></n-gi>
       <n-gi>
-        <n-image preview-disabled width="64.2" src="index\neural 1.png" />
+
       </n-gi>
       <n-gi span="1"></n-gi>
       <n-gi :span="5">
         <div style="height: 5px"></div>
-        <n-image
+        <p
           @click="routerToIndex"
-          preview-disabled
-          width="257"
-          src="index\top nav bar.png"
-        />
+
+        >返回</p>
       </n-gi>
       <n-gi span="2"> </n-gi>
 
