5ec5fc6150c4899966682b2f96e3bc4e905ae443
gong-wenxin <1153712410@qq.com>
2022-08-05 14:09:35 +0800

    gwx

--- src/views/project/index.vue
+++ src/views/project/index.vue
@@ -5,17 +5,9 @@
       <n-gi span="7">
 
       </n-gi>
-<!-- v-model:value="" -->
+      <!-- v-model:value="" -->
       <n-gi span="5">
-        <n-input
-            
-            ref="inputInstRef"
-            placeholder="搜索项目"
-            clearable
-            round
-            size="large"
-            style="margin-left: 19px"
-        >
+        <n-input ref="inputInstRef" placeholder="搜索项目" clearable round size="large" style="margin-left: 19px">
           <template #prefix>
             <n-image src="svg\project_svg\search.svg"></n-image>
           </template>
@@ -24,12 +16,7 @@
       <n-gi span="1">
       </n-gi>
       <n-gi span="3">
-        <el-button
-            round
-            color="#2772F0"
-            size="large"
-            @click="dialogCreateVisible = true"
-        >
+        <el-button round color="#2772F0" size="large" @click="dialogCreateVisible = true">
           <template #icon>
             <n-icon>
               <n-image src="svg\project_svg\plus.svg" />
@@ -52,20 +39,20 @@
     </div>
 
     <n-grid x-gap="5" :cols="30">
-      
+
       <n-gi span="1">
       </n-gi>
 
       <n-gi span="28">
-          <n-layout  class="bg"  bordered="false" content-style="padding: 30px 30px 30px 30px;">
-        <n-collapse >
-          <template #arrow>
-            <n-icon>
-              <n-image preview-disabled src="svg\project_svg\folder.svg" />
-            </n-icon>
-          </template>
-          <n-collapse-item title="&emsp;全部项目"  >
-            <n-divider></n-divider>
+        <n-layout class="bg" bordered="false" content-style="padding: 30px 30px 30px 30px;">
+          <n-collapse>
+            <template #arrow>
+              <n-icon>
+                <n-image preview-disabled src="svg\project_svg\folder.svg" />
+              </n-icon>
+            </template>
+            <n-collapse-item title="&emsp;全部项目">
+              <n-divider></n-divider>
               <!-- <el-table  style="width: 100%">
                 <el-table-column prop="obname" label="项目名称" width="280" />
                 <el-table-column prop="obstate" label="项目描述" width="580" />
@@ -73,117 +60,112 @@
               </el-table> -->
 
               <!--加载项目-->
-              <Card v-for="(project,index) in projects"
-              :key="project.project_id"
-              :one-project="project"
-             ></Card>
-            <!-- <n-grid x-gap="20px" y-gap="20px" cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
+              <Card v-for="(project, index) in projects" :key="project.project_id" :one-project="project"></Card>
+              <!-- <n-grid x-gap="20px" y-gap="20px" cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
               <n-grid-item>
                 <Card></Card>
               </n-grid-item>
             </n-grid> -->
 
-            <template #header-extra>
-              <n-image preview-disabled src="svg\project_svg\sort.svg" />
-            </template>
-          </n-collapse-item>
-        </n-collapse>
-      </n-layout>
+              <template #header-extra>
+                <n-image preview-disabled src="svg\project_svg\sort.svg" />
+              </template>
+            </n-collapse-item>
+          </n-collapse>
+        </n-layout>
       </n-gi>
       <n-gi span="1">
       </n-gi>
-      
+
     </n-grid>
 
     <n-grid x-gap="5" :cols="30">
-      
+
       <n-gi span="1">
       </n-gi>
 
       <n-gi span="28">
-          <n-layout  class="bg"  bordered="false" content-style="padding: 30px 30px 30px 30px;">
-        <n-collapse >
-          <template #arrow>
-            <n-icon>
-              <n-image preview-disabled src="svg\project_svg\folder.svg" />
-            </n-icon>
-          </template>
-          <n-collapse-item title="&emsp;我创建的"  >
-            <n-divider></n-divider>
-
-            <n-grid x-gap="20px" y-gap="20px" cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
-              <n-grid-item>
-                <Card></Card>
-              </n-grid-item>
-            </n-grid>
-
-            <template #header-extra>
-              <n-image preview-disabled src="svg\project_svg\sort.svg" />
+        <n-layout class="bg" bordered="false" content-style="padding: 30px 30px 30px 30px;">
+          <n-collapse>
+            <template #arrow>
+              <n-icon>
+                <n-image preview-disabled src="svg\project_svg\folder.svg" />
+              </n-icon>
             </template>
-          </n-collapse-item>
-        </n-collapse>
-      </n-layout>
+            <n-collapse-item title="&emsp;我创建的">
+              <n-divider></n-divider>
+
+              <n-grid x-gap="20px" y-gap="20px" cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
+                <n-grid-item>
+                  <Card></Card>
+                </n-grid-item>
+              </n-grid>
+
+              <template #header-extra>
+                <n-image preview-disabled src="svg\project_svg\sort.svg" />
+              </template>
+            </n-collapse-item>
+          </n-collapse>
+        </n-layout>
       </n-gi>
       <n-gi span="1">
       </n-gi>
-      
+
     </n-grid>
 
     <n-grid x-gap="5" :cols="30">
-      
+
       <n-gi span="1">
       </n-gi>
 
       <n-gi span="28">
-          <n-layout  class="bg"  bordered="false" content-style="padding: 30px 30px 30px 30px;">
-        <n-collapse >
-          <template #arrow>
-            <n-icon>
-              <n-image preview-disabled src="svg\project_svg\folder.svg" />
-            </n-icon>
-          </template>
-          <n-collapse-item title="&emsp;我参与的"  >
-            <n-divider></n-divider>
-
-            <n-grid x-gap="20px" y-gap="20px" cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
-              <n-grid-item>
-                <Card></Card>
-              </n-grid-item>
-            </n-grid> 
-
-            <template #header-extra>
-              <n-image preview-disabled src="svg\project_svg\sort.svg" />
+        <n-layout class="bg" bordered="false" content-style="padding: 30px 30px 30px 30px;">
+          <n-collapse>
+            <template #arrow>
+              <n-icon>
+                <n-image preview-disabled src="svg\project_svg\folder.svg" />
+              </n-icon>
             </template>
-          </n-collapse-item>
-        </n-collapse>
-      </n-layout>
+            <n-collapse-item title="&emsp;我参与的">
+              <n-divider></n-divider>
+
+              <n-grid x-gap="20px" y-gap="20px" cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
+                <n-grid-item>
+                  <Card></Card>
+                </n-grid-item>
+              </n-grid>
+
+              <template #header-extra>
+                <n-image preview-disabled src="svg\project_svg\sort.svg" />
+              </template>
+            </n-collapse-item>
+          </n-collapse>
+        </n-layout>
       </n-gi>
       <n-gi span="1">
       </n-gi>
-      
+
     </n-grid>
 
   </n-space>
 
   <!--编辑信息dialog-->
-   <el-dialog v-model="dialogCreateVisible" title="新建项目信息">
-                <el-form :model="form">
-                <el-form-item label="项目名称" :label-width="formLabelWidth">
-                    <el-input v-model="form.name" autocomplete="off" placeholder="请输入项目名称"/>
-                </el-form-item>
-                <el-form-item label="项目描述" :label-width="formLabelWidth">
-                    <el-input v-model="form.region" placeholder="请输入项目描述"/>
-                </el-form-item>
-                </el-form>
-                <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="dialogCreateVisible = false">取消</el-button>
-                    <el-button type="primary"  @click="project_create() "
-                    >确定</el-button
-                    >
-                </span>
-                </template>
-            </el-dialog>
+  <el-dialog v-model="dialogCreateVisible" title="新建项目信息">
+    <el-form :model="form">
+      <el-form-item label="项目名称" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off" placeholder="请输入项目名称" />
+      </el-form-item>
+      <el-form-item label="项目描述" :label-width="formLabelWidth">
+        <el-input v-model="form.region" placeholder="请输入项目描述" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogCreateVisible = false">取消</el-button>
+        <el-button type="primary" @click="project_create()">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
 
 </template>
 
@@ -227,18 +209,18 @@ onMounted(() => {
   getProject();
   console.log("1");
 });
- 
+
 let count: number = 0;
-let one_group_id : number;
+let one_group_id: number;
 const projects: any[] = reactive([]);
 //获取项目
 const getProject = (clear: boolean = true) => {
-   axios({
+  axios({
     url: axios.defaults.baseURL + "/group/get_groups",
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      "Authorization":User.token
+      "Authorization": User.token
     },
     data: {
     },
@@ -250,11 +232,11 @@ const getProject = (clear: boolean = true) => {
       },
     ],
   }).then(function (response) {
-      // 处理成功情况
-      if (response.data?.success) {
-    one_group_id=response.data.groups[0].GroupID
-        console.log("groupsID",response.data.groups[0].GroupID);
-      }
+    // 处理成功情况
+    if (response.data?.success) {
+      one_group_id = response.data.groups[0].GroupID
+      console.log("groupsID", response.data.groups[0].GroupID);
+    }
   })
   //   section.value=parseInt(localStorage.getItem("section")+"")
   axios({
@@ -262,7 +244,7 @@ const getProject = (clear: boolean = true) => {
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      "Authorization":User.token
+      "Authorization": User.token
     },
     data: {
     },
@@ -274,73 +256,74 @@ const getProject = (clear: boolean = true) => {
       },
     ],
   }).then(function (response) {
-      // 处理成功情况
-      if (response.data?.success) {
-        count = response.data?.count;
-        console.log(response.data);
-        let i = 0;
-        if (clear) while (projects.length != 0) projects.pop();
-        if (response.data!= null)
-          one_group_id = response.data.groups[0].groupID;
-          console.log("one_group_id"+ one_group_id);
-        // User.Name=modelRef.value.name,
-        // User.Id=response.data.data.user_id,
-      } else {
-      }
+    // 处理成功情况
+    if (response.data?.success) {
+      count = response.data?.count;
       console.log(response.data);
+      let i = 0;
+      if (clear) while (projects.length != 0) projects.pop();
+      if (response.data != null)
+      one_group_id = response.data.groups[0].groupID;
+      console.log("one_group_id" + one_group_id);
+      axios({
+        url: axios.defaults.baseURL + "/proj/get_proj_all",
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": User.token
+        },
+        data: {
+          group_id: one_group_id
+        },
+        transformRequest: [
+          function (data, headers) {
+            let data1 = JSON.stringify(data);
+            console.log(data1);
+            return data1;
+          },
+        ],
+      }).then(function (response) {
+        // 处理成功情况
+        if (response.data?.success) {
+          count = response.data?.count;
+          console.log(response.data);
+          let i = 0;
+          if (clear) while (projects.length != 0) projects.pop();
+          if (response.data != null)
+            for (i = 0; i < count; i++) {
+              let temp = reactive({
+                group_id: response.data.projs[i].group_id,
+                proj_id: response.data.projs[i].proj_id,
+                proj_info: response.data.projs[i].proj_info,
+                proj_name: response.data.projs[i].proj_name,
+                status: response.data.projs[i].status,
+                user_id: response.data.projs[i].user_id,
+              });
+              projects.push(temp);
+            }
+          console.log(projects);
+          // User.Name=modelRef.value.name,
+          // User.Id=response.data.data.user_id,
+        } else {
+        }
+        console.log(response.data);
+      });
+      // User.Name=modelRef.value.name,
+      // User.Id=response.data.data.user_id,
+    } else {
+    }
+    console.log(response.data);
   });
-    axios({
-    url: axios.defaults.baseURL + "/proj/get_proj_all",
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization":User.token
-    },
-    data: {
-      group_id : one_group_id
-    },
-    transformRequest: [
-      function (data, headers) {
-        let data1 = JSON.stringify(data);
-        console.log(data1);
-        return data1;
-      },
-    ],
-  }).then(function (response) {
-      // 处理成功情况
-      if (response.data?.success) {
-        count = response.data?.count;
-        console.log(response.data);
-        let i = 0;
-        if (clear) while (projects.length != 0) projects.pop();
-        if (response.data!= null)
-          for (i = 0; i < count; i++) {
-            let temp = reactive({
-              group_id:response.data.projs[i].group_id,
-              proj_id: response.data.projs[i].proj_id,
-              proj_info: response.data.projs[i].proj_info,
-              proj_name: response.data.projs[i].proj_name,
-              status: response.data.projs[i].status,
-              user_id: response.data.projs[i].user_id,
-            });
-            projects.push(temp);
-          }
-        console.log(projects);
-        // User.Name=modelRef.value.name,
-        // User.Id=response.data.data.user_id,
-      } else {
-      }
-      console.log(response.data);
-    });
+
 }
 //创建项目
-const project_create = () =>{
+const project_create = () => {
   axios({
     url: axios.defaults.baseURL + "/proj/create_proj",
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      "Authorization":User.token
+      "Authorization": User.token
     },
     data: {
       // post_id: props.onePost.post_id,
@@ -360,20 +343,20 @@ const project_create = () =>{
     ],
   }).then(function (response) {
     // 处理成功情况
-      console.log(response.data);
-      if (response.data?.success) {
-        message.success("创建成功");
-        getProject();
-        console.log("2");
-        dialogCreateVisible.value = false;
+    console.log(response.data);
+    if (response.data?.success) {
+      message.success("创建成功");
+      getProject();
+      console.log("2");
+      dialogCreateVisible.value = false;
 
-        // setTimeout(() => {
-        //   footRef.value.style.width = post.value?.offsetWidth + "px";
-        // }, 200);
-      } else {
-        message.error(response.data?.message);
-      }
-      console.log(response.data);
+      // setTimeout(() => {
+      //   footRef.value.style.width = post.value?.offsetWidth + "px";
+      // }, 200);
+    } else {
+      message.error(response.data?.message);
+    }
+    console.log(response.data);
   });
 }
 
@@ -382,66 +365,73 @@ const project_create = () =>{
 
 
 <style scoped>
-  .headTitle {
-    margin: auto;
-    color: #000000;
-    vertical-align: middle;
-    font-family: nunito-sans, sans-serif;
-    font-size: 24px;
-    font-weight: bold;
-  }
-  .commonText {
-    color: #FFFFFF;
-    font-family: Roboto;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: normal;
-    letter-spacing: 2px;
-    text-align: left;
-  }
-  .bg {
-    /* opacity: 0.10000000149011612; */
-    border-radius: 16px;
-    background: rgb(255,255,255);
-    text-align: center;
-  }
-  .projCard {
-    max-width: 300px;
-  }
-  .state{
-    opacity: 0.4000000059604645;
-    color: #091B3D;
-    font-family: Sk-Modernist;
-    font-weight: regular;
-    font-size: 12px;
-    line-height: 16px;
-    letter-spacing: 0px;
-    text-align: left;
+.headTitle {
+  margin: auto;
+  color: #000000;
+  vertical-align: middle;
+  font-family: nunito-sans, sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+}
 
-  }
-  .name{
-    color: #091B3D;
-    font-family: Sk-Modernist;
-    font-weight: bold;
-    font-size: 16px;
-    line-height: 26px;
-    letter-spacing: 0px;
-    text-align: left;
+.commonText {
+  color: #FFFFFF;
+  font-family: Roboto;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: normal;
+  letter-spacing: 2px;
+  text-align: left;
+}
 
-  }
-  .user {
-    /* opacity: 0.10000000149011612; */
-    border-radius: 16px;
-    /*background: rgb(245, 181, 68, 0.1);*/
-    text-align: center;
-  }
-  .buttonText2 {
-    color: #2772F0;
-    font-family: nunito-sans, sans-serif;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: normal;
-    letter-spacing: 2px;
-    text-align: left;
-  }
+.bg {
+  /* opacity: 0.10000000149011612; */
+  border-radius: 16px;
+  background: rgb(255, 255, 255);
+  text-align: center;
+}
+
+.projCard {
+  max-width: 300px;
+}
+
+.state {
+  opacity: 0.4000000059604645;
+  color: #091B3D;
+  font-family: Sk-Modernist;
+  font-weight: regular;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0px;
+  text-align: left;
+
+}
+
+.name {
+  color: #091B3D;
+  font-family: Sk-Modernist;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 26px;
+  letter-spacing: 0px;
+  text-align: left;
+
+}
+
+.user {
+  /* opacity: 0.10000000149011612; */
+  border-radius: 16px;
+  /*background: rgb(245, 181, 68, 0.1);*/
+  text-align: center;
+}
+
+.buttonText2 {
+  color: #2772F0;
+  font-family: nunito-sans, sans-serif;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: normal;
+  letter-spacing: 2px;
+  text-align: left;
+}
 </style>
