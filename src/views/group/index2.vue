24120595f21a3b0ff6e8cc83558c83d02cd8daf4
周骁睿 <2651419563@qq.com>
2022-08-08 22:28:47 +0800

    update

--- src/views/group/index2.vue
+++ src/views/group/index2.vue
@@ -6,17 +6,18 @@
 
       </n-gi>
       <n-gi span="1">
-        <div id="group_name">团队名称</div>
+        <n-image src="svg\group_svg\home.svg" />
+        <span id="group_name">团队名称</span>
+
       </n-gi>
-      <n-gi span="1">
-        <tk-select selected="请选择">
-          <template #selectDropDown>
-            <tk-select-item value="最新案例">最新案例</tk-select-item>
-            <tk-select-item value="最热案例">最热案例</tk-select-item>
-          </template>
-        </tk-select>
+      <n-gi span="2">
+
       </n-gi>
-      <n-gi span="4">
+      <n-gi span="3">
+        <el-button @click="handleClick()" class="add_group">
+          <n-image src="svg\group_svg\add.svg" class="add_group_img"/>
+          <span class="add_group_word">新建团队</span>
+        </el-button>
 
       </n-gi>
       <!-- v-model:value="" -->
@@ -55,11 +56,7 @@
       <div class="title_font forth"></div>
       <div class="title_font fifth"></div>
     </div>
-    
-    <Bar></Bar>
-    
-
-    <!-- <ul class="group_mem">
+    <ul class="group_mem">
       <li v-for="(todo, index) in todos" :key="todo.name">
         <div class="person">
           <input type="checkbox" v-model="todo.done" class="tip_checkbox">
@@ -72,200 +69,134 @@
           <span class="fifth tip_font" @click="changeTode(index)">
             <n-image src="svg\group_svg\manage.svg" />
           </span>
+          <!--          <span v-if="changeDemo.show">-->
+          <!--            <input type="text" v-model="changeDemo.val" class="tip_modify">-->
+          <!--            <button @click="saveChange">保存</button>-->
+          <!--          </span>-->
         </div>
       </li>
-    </ul> -->
-    <!-- <div id="del_all">
+    </ul>
+    <div id="del_all">
       <input type="checkbox" v-model="isAll" id="del_checkbox">
       <span class="del_font">全选</span>
       <span class="del_font">{{ selNum }}/{{ len }}</span>
       <button @click="delAll" id="del_btn">删除</button>
-    </div> -->
+    </div>
   </div>
 
 </template>
 
 <script lang="ts" setup>
-import {
-  onUpdated,
-  toRaw,
-  reactive,
-  onBeforeMount,
-  ref,
-  onMounted,
-  StyleValue,
-  Ref,
-  getCurrentInstance,
-  h,
-} from "vue";
-import Bar from "../../components/member.vue";
-import axios from "axios";
-// import { usememberstore } from "../../store/Project";
-import { useUserStore } from "../../store/User";
-import { useDialog,InputInst, useMessage } from "naive-ui";
+import { computed, ref } from "vue";
+import { defineComponent } from 'vue'
+import { useMessage } from 'naive-ui'
+import { useRouter} from 'vue-router'
 
-const User = useUserStore();
-let count: number = 0;
-let one_group_id: number;
-const members: any[] = reactive([]);
-
-onBeforeMount(() => {
-  getMembers();
-  console.log("1");
-});
-
-//获取项目
-const getMembers = (clear: boolean = true) => {
-  axios({
-    url: axios.defaults.baseURL + "/group/get_groups",
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": User.token
-    },
-    data: {
-    },
-    transformRequest: [
-      function (data, headers) {
-        let data1 = JSON.stringify(data);
-        console.log(data1);
-        return data1;
-      },
-    ],
-  }).then(function (response) {
-    // 处理成功情况
-    if (response.data?.success) {
-      count = response.data?.count;
-      console.log(response.data);
-      let i = 0;
-      if (clear) while (members.length != 0) members.pop();
-      if (response.data != null)
-      one_group_id = response.data.groups[0].group_id;
-      console.log("one_group_id" + one_group_id);
-      axios({
-        url: axios.defaults.baseURL + "/proj/get_group_members",
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": User.token
-        },
-        data: {
-          group_id: one_group_id,
-        },
-        transformRequest: [
-          function (data, headers) {
-            let data1 = JSON.stringify(data);
-            console.log(data1);
-            return data1;
-          },
-        ],
-      }).then(function (response) {
-        // 处理成功情况
-        if (response.data?.success) {
-          count = response.data?.count;
-          console.log(response.data);
-          let i = 0;
-          if (clear) while (members.length != 0) members.pop();
-          if (response.data != null)
-            for (i = 0; i < count; i++) {
-              let temp = reactive({
-                email: response.data.members[i].email,
-                real_name: response.data.members[i].real_name,
-                status: response.data.members[i].status,
-                user_id: response.data.members[i].user_id,
-                username: response.data.members[i].username,
-              });
-              // console.log("  projectid" + temp.proj_id);
-              members.push(temp);
-            }
-          console.log(members);
-          // User.Name=modelRef.value.name,
-          // User.Id=response.data.data.user_id,
-        } else {
-        }
-        console.log(response.data);
-      });
-      // User.Name=modelRef.value.name,
-      // User.Id=response.data.data.user_id,
-    } else {
-    }
-    console.log(response.data);
-  });
-
+const router = useRouter()
+const handleClick = () => {
+  router.push({
+    path: 'NewGroup',
+  })
 }
 
-
-// interface todo {
-//   done: boolean,
-//   name: string,
-//   email: string,
-//   identity: string,
-// }
-// let val = ref('')
-// let todos = ref<todo[]>([])
-// // 增
-// const addTodo=function() {
-//   if (val.value) {
-//     todos.value.push({
-//       done: false,
-//       name: val.value,
-//       email: val.value+"@buaa.edu.cn",
-//       identity: "普通成员",
-//     })
-//     val.value = ''
-//   }
-// }
-// // 删
-// function deltodo(index: number) {
-//   todos.value.splice(index, 1)
-// }
-// // 全选功能实现
-// let len = computed<number>(() => todos.value.length)
-// let selNum = computed<number>(() => todos.value.filter(v => v.done).length)
-// let isAll = computed<boolean>({
-//   get() {
-//     return len.value === 0 ? false : len.value == selNum.value
-//   },
-//   set(val: boolean) {
-//     todos.value.forEach(v => { v.done = val })
-//   }
-// })
-// let changeDemo = ref({
-//   show: false,
-//   name: '',
-//   val: '管理员',
-//   changeIndex: -1
-// })
-// // 改
-// function changeTode(i: number) {
-//   const demo = todos.value[i]
-//   changeDemo.value = {
-//     show: true,
-//     name: demo.name,
-//     val: '管理员',
-//     changeIndex: i
-//   }
-//   saveChange();
-// }
-// function saveChange() {
-//   let { changeIndex, val } = changeDemo.value
-//   todos.value[changeIndex].identity = val
-//   changeDemo.value = {
-//     show: false,
-//     name: '',
-//     val: '',
-//     changeIndex: -1
-//   }
-// }
-// // 全选删除
-// function delAll() {
-//   if (isAll.value) {
-//     todos.value = []
-//   }
-// }
+interface todo {
+  done: boolean,
+  name: string,
+  email: string,
+  identity: string,
+}
+let val = ref('')
+let todos = ref<todo[]>([])
+// 增
+const addTodo=function() {
+  if (val.value) {
+    todos.value.push({
+      done: false,
+      name: val.value,
+      email: val.value+"@buaa.edu.cn",
+      identity: "普通成员",
+    })
+    val.value = ''
+  }
+}
+// 删
+function deltodo(index: number) {
+  todos.value.splice(index, 1)
+}
+// 全选功能实现
+let len = computed<number>(() => todos.value.length)
+let selNum = computed<number>(() => todos.value.filter(v => v.done).length)
+let isAll = computed<boolean>({
+  get() {
+    return len.value === 0 ? false : len.value == selNum.value
+    },
+  set(val: boolean) {
+    todos.value.forEach(v => { v.done = val })
+            }
+})
+let changeDemo = ref({
+  show: false,
+  name: '',
+  val: '管理员',
+  changeIndex: -1
+})
+// 改
+function changeTode(i: number) {
+  const demo = todos.value[i]
+  changeDemo.value = {
+    show: true,
+    name: demo.name,
+    val: '管理员',
+    changeIndex: i
+        }
+  saveChange();
+    }
+function saveChange() {
+  let { changeIndex, val } = changeDemo.value
+  todos.value[changeIndex].identity = val
+  changeDemo.value = {
+    show: false,
+    name: '',
+    val: '',
+    changeIndex: -1
+}
+}
+// 全选删除
+function delAll() {
+  if (isAll.value) {
+    todos.value = []
+  }
+}
 </script>
 
 <style>
+
+.add_group{
+  height: 30px !important;
+  border: none !important;
+  background-color: transparent !important;
+}
+
+.add_group_img{
+  height: 30px;
+  width: 30px;
+}
+
+.add_group_word{
+  margin-left: 32px;
+  color: #000000;
+  font-family: Inria Sans;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 24px;
+  letter-spacing: 0px;
+  text-align: left;
+  position: absolute;
+}
+
+.add_group_word:hover{
+  color: #F5B544 !important;
+}
 
 #group_up_blank{
   height: 30px;
@@ -418,3 +349,4 @@ const getMembers = (clear: boolean = true) => {
 
 
 </style>
+
