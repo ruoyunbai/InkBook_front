0b18e4197a29917766b9418ca678ba65e063bf8e
gong-wenxin <1153712410@qq.com>
2022-08-09 15:57:16 +0800

    auto push

--- src/views/graph/index.vue
+++ src/views/graph/index.vue
@@ -1,20 +1,40 @@
 <template>
 <div ref="root">
-    <iframe id="inlineFrameExample"
+    <iframe
+    ref="ifr"
+    id="inlineFrameExample"
     title="Inline Frame Example"
     :width="height"
     :height="width"
     class="ifra"
-    src="https://www.iodraw.com/diagram/">
+    :src=url
+    @load="iframeLoad"
+    >
 </iframe>
 </div>
 </template>
 <script lang="ts" setup>
 import {ref,onMounted} from 'vue'
+const url=ref("https://embed.diagrams.net/?embed=1&proto=json&spin=1&libraries=1&noSaveBtn=1")
 // import 
+// "https://www.iodraw.com/diagram/"
+const ifr=ref()
 const height=ref(-1)
 const width=ref(-1)
 const root=ref(null)
+const iframeLoad=()=>{
+    window.addEventListener("message",(e)=>{
+        console.log(e)
+        if(JSON.parse(e.data).event=="init"){
+            console.log("event:init")
+            if(ifr!=null){
+                console.log(ifr.value)
+                ifr.value.contentWindow.postMessage(JSON.stringify({action:"load",
+                xml:""}), "*");
+            }
+        }
+    });
+}
 onMounted(()=>{
     // height.value=document.body.offsetHeight
     // window.screen.availHeight;
