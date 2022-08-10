c61105777c99a6f18f26f535a3cdcd52ba7ce643
gong-wenxin <1153712410@qq.com>
2022-08-10 09:48:55 +0800

    auto push

--- src/views/docCenter/index.vue
+++ src/views/docCenter/index.vue
@@ -1,38 +1,46 @@
 <template>
+ <div class="center_head">
+    <n-grid x-gap="12" :cols="16">
+      <n-gi span="1">
+      </n-gi>
+      <n-gi span="3">
+        <n-image src="svg\center_svg\title_img.svg" />
+        <span class="title_font">团队文档中心</span>
+      </n-gi>
+      <n-gi span="5">
+      </n-gi>
+      <n-gi span="1">
+        <n-button
+          color="#2772F0"
+          text-color="#FFFFFF"
+          style="width: 130px; height: 46px; border-radius: 16px;"
+        >
+          <n-image src="svg\center_svg\export.svg" />
+          <span class="btn_font">导出文档</span>
+        </n-button>
+      </n-gi>
+      <n-gi span="1">
 
-    <div>
-        <n-modal v-model:show="showModal" style="width:500px" class="custom-card" preset="card" title=" 请输入文档名字"
-            size="huge" :bordered="false">
-            <template #header-extra>
-                取消
-            </template>
-            <n-input v-model:value="createName"></n-input>
-            <template #footer>
-                <n-button>确定</n-button>
-            </template>
-        </n-modal>
-        <!-- <n-card :bordered="false" > -->
+      </n-gi>
+      <n-gi span="1">
+        <n-button
+            color="#2772F0"
+            text-color="#FFFFFF"
+            style="width: 130px; height: 46px; border-radius: 16px;"
+        >
+          <n-image src="svg\center_svg\more.svg" />
+          <span class="btn_font">更多模板</span>
+        </n-button>
+      </n-gi>
+    </n-grid>
+  </div>
+  <div class="center_mid">
+    <div class="center_edit">
 
-        <n-grid :cols="100">
-            <n-gi></n-gi>
-            <n-gi span="3">
-                <n-image preview-disabled src="svg\doc\形状 (1).svg"></n-image>
-            </n-gi>
-            <n-gi span="15">
-                <div style="font-family: 'Roboto';
-font-style: normal;
-font-weight: 700;
-font-size: 32px;
-line-height: 34px;
-color: #000000;">团队文档中心</div>
-            </n-gi>
-        </n-grid>
-        <div style="height:20px"></div>
-        <n-grid cols="20">
-            <n-gi span="15"></n-gi>
-            <n-gi span="5">
-                <div class="tree">
-                    <div class="custom-tree-container">
+    </div>
+    <div class="center_menu">
+      <n-space vertical :size="12">
+       <div class="custom-tree-container">
                         <!-- <p>Using render-content</p>
                         <el-tree :data="dataSource" show-checkbox node-key="id" default-expand-all
                             :expand-on-click-node="false" :render-content="renderContent" />
@@ -53,10 +61,57 @@ color: #000000;">团队文档中心</div>
                             </template>
                         </el-tree>
                     </div>
-                </div>
-            </n-gi>
+        <!-- <n-tree
+            :default-expand-all="true"
+            :show-irrelevant-nodes="showIrrelevantNodes"
+            :pattern="pattern"
+            :data="data"
+            :render-prefix="renderPrefix"
+            block-line
+        /> -->
+      </n-space>
+    </div>
+    <div class="add_btn">
+      <n-button
+          color="rgba(39, 114, 240, 0.09)"
+          text-color="#2772F0"
+          style="position: absolute; width: 169px; height: 32px; border-radius: 20px"
+      >
+        <span class="add_btn_font">+ 设计项目文档区</span>
+      </n-button>
+    </div>
+  </div>
 
-        </n-grid>
+
+
+    <div>
+        <n-modal v-model:show="showModal" style="width:500px" class="custom-card" preset="card" title=" 请输入文档名字"
+            size="huge" :bordered="false">
+            <template #header-extra>
+                取消
+            </template>
+            <n-input v-model:value="createName"></n-input>
+            <template #footer>
+                <n-button>确定</n-button>
+            </template>
+        </n-modal>
+        <!-- <n-card :bordered="false" > -->
+
+        <!-- <n-grid :cols="100">
+            <n-gi></n-gi>
+            <n-gi span="3">
+                <n-image preview-disabled src="svg\doc\形状 (1).svg"></n-image>
+            </n-gi>
+            <n-gi span="15">
+                <div style="font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 34px;
+color: #000000;">团队文档中心</div>
+            </n-gi>
+        </n-grid> -->
+
     </div>
 </template>
 <script setup lang="ts">
@@ -72,7 +127,6 @@ interface Tree {
   children?: Tree[]
 }
 let id = 1000
-
 const append = (data: Tree) => {
     
     const newChild = { id: id++, label: 'testtest', children: [] }
@@ -176,124 +230,13 @@ const dataSource = ref<Tree[]>([
     border-radius: 24px;
     height: 856px;
 }
-=======
-  <div class="center_head">
-    <n-grid x-gap="12" :cols="16">
-      <n-gi span="1">
-      </n-gi>
-      <n-gi span="3">
-        <n-image src="svg\center_svg\title_img.svg" />
-        <span class="title_font">团队文档中心</span>
-      </n-gi>
-      <n-gi span="5">
-      </n-gi>
-      <n-gi span="1">
-        <n-button
-          color="#2772F0"
-          text-color="#FFFFFF"
-          style="width: 130px; height: 46px; border-radius: 16px;"
-        >
-          <n-image src="svg\center_svg\export.svg" />
-          <span class="btn_font">导出文档</span>
-        </n-button>
-      </n-gi>
-      <n-gi span="1">
+</style>
 
-      </n-gi>
-      <n-gi span="1">
-        <n-button
-            color="#2772F0"
-            text-color="#FFFFFF"
-            style="width: 130px; height: 46px; border-radius: 16px;"
-        >
-          <n-image src="svg\center_svg\more.svg" />
-          <span class="btn_font">更多模板</span>
-        </n-button>
-      </n-gi>
-    </n-grid>
-  </div>
-  <div class="center_mid">
-    <div class="center_edit">
 
-    </div>
-    <div class="center_menu">
-      <n-space vertical :size="12">
-        <n-tree
-            :default-expand-all="true"
-            :show-irrelevant-nodes="showIrrelevantNodes"
-            :pattern="pattern"
-            :data="data"
-            :render-prefix="renderPrefix"
-            block-line
-        />
-      </n-space>
-    </div>
-    <div class="add_btn">
-      <n-button
-          color="rgba(39, 114, 240, 0.09)"
-          text-color="#2772F0"
-          style="position: absolute; width: 169px; height: 32px; border-radius: 20px"
-      >
-        <span class="add_btn_font">+ 设计项目文档区</span>
-      </n-button>
-    </div>
-  </div>
-</template>
 
-<script lang="ts">
-import { h, defineComponent, ref } from 'vue'
-import { NButton, TreeOption } from 'naive-ui'
-import { repeat } from 'seemly'
-import dirImg from "../../../public/svg/center_svg/dir.svg"
-import fileImg from "../../../public/svg/center_svg/file.svg"
 
-const data: TreeOption[] = [
-  {
-    label: '项目文件',
-    key: '0',
-    children: [
-      {
-        label: '项目1',
-        key: '0-0',
-        children: [
-          { label: '文档1', key: '0-0-0' },
-          { label: '文档2', key: '0-0-1' }
-        ]
-      },
-      {
-        label: '项目2',
-        key: '0-1',
-        children: [
-          { label: '文档1', key: '0-1-0' },
-          { label: '文档2', key: '0-1-1' }
-        ]
-      }
-    ]
-  },
-]
 
-export default defineComponent({
-  setup () {
-    return {
-      data,
-      pattern: ref(''),
-      showIrrelevantNodes: ref(false),
-      dirImg,
-      fileImg,
-      renderPrefix,
-    }
-  }
-})
 
-function renderPrefix ({ option }: { option: TreeOption }) {
-  return h(
-      NButton,
-      { text: true, type: 'primary' },
-      { default: () => 'img'}
-  )
-}
-
-</script>
 
 <style>
 
@@ -357,6 +300,4 @@ function renderPrefix ({ option }: { option: TreeOption }) {
   margin-left: 76%;
   margin-top: 750px;
 }
-
->>>>>>> 95bfcbb8884e23440b9859d7259f74df02ddd1cb
 </style>
